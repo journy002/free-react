4dfd90255c72985cc831ff52f515771cd4fa23f2
journy002 <journy002@naver.com>
2022-03-11 17:06:08 +0900

    make what i want to make

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let [title, setTitle] = useState([
+    "남자 코트 추천",
+    "강남 우동맛집",
+    "파이썬 독학",
+  ]);
+
+  let [count, setCount] = useState(0);
+
+  const clickGood = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  const changeCol = () => {
+    let newArray = [...title];
+    newArray[0] = "여자 코트 추천";
+    setTitle(newArray);
+  };
+
+  const reverseFun = () => {
+    let newArray = [...title];
+    newArray.sort();
+    setTitle(newArray);
+    console.log(newArray);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="container">
+        <div>GOGO DEVELOPMENT</div>
+        <button
+          onClick={() => {
+            reverseFun();
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          정렬
+        </button>
+        <ul className="list_box">
+          <li>
+            <div>
+              <strong>{title[0]}</strong>
+              <span className="good" onClick={() => clickGood()}>
+                👍
+              </span>
+              <span className="goodCount">{count}</span>
+            </div>
+            <div>2월 18일 발행</div>
+            <button
+              onClick={() => {
+                changeCol();
+              }}
+              className="change_col"
+            >
+              Change Collection
+            </button>
+          </li>
+          <li>
+            <div>
+              <strong>{title[1]}</strong>
+              <span className="good" onClick={() => clickGood()}>
+                👍
+              </span>
+              <span className="goodCount">{count}</span>
+            </div>
+            <div>2월 18일 발행</div>
+          </li>
+          <li>
+            <div>
+              <strong>{title[2]}</strong>
+              <span className="good" onClick={() => clickGood()}>
+                👍
+              </span>
+              <span className="goodCount">{count}</span>
+            </div>
+            <div>2월 18일 발행</div>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 }
 
